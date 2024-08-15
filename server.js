80c95e9c28454b8e608577e7b9e069bdffd0e7a1
Muthana6 <marvelge009@gmail.com>
2024-08-15 23:51:35 +0200

    ready

--- server.js
+++ server.js
@@ -38,12 +38,10 @@ app.use('/api/v1/jobs',authenticateUser ,jobRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users',authenticateUser ,userRouter)
 
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use('/api/v1/jobs' ,jobRouter)
-// app.use('/api/v1/auth', authRouter)
-// app.use('/api/v1/users',userRouter)
 
+app.get('*',(req, res)=> {
+    res.sendFile(path.resolve(__dirname,'./public', 'index.html'))
+})
 
 
 app.get('/api/v1/test', (req, res) => {
